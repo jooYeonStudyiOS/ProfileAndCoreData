16c75a5d6ab08b36e8c11b4c68c312ab54b7e6dd
jooYeon <conn0204@naver.com>
2023-09-21 16:08:04 +0900

    profile design view 추가

---
+++ ProfileAndCoreData/ProfileAndCoreData/Profile/View/ProfileDesignViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileDesignViewController.swift
+//  ProfileAndCoreData
+//
+//  Created by Jooyeon Kang on 2023/09/21.
+//
+
+import UIKit
+
+class ProfileDesignViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- ProfileAndCoreData/ProfileAndCoreData/Profile/View/ProfileMainViewController.swift
+++ ProfileAndCoreData/ProfileAndCoreData/Profile/View/ProfileMainViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ProfileMainViewController: UIViewController {
+    @IBAction func didTappedProfileDesignButton(_ sender: Any) {
+        if let navigationController = storyboard?.instantiateViewController(withIdentifier: "NavigationController") as? UINavigationController {
+            navigationController.modalPresentationStyle = .fullScreen
+            present(navigationController, animated: true, completion: nil)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
