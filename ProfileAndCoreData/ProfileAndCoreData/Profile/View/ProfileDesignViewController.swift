12f468525ccc394e50ab0bbdd6ae96b2008c3ef3
jooYeon <conn0204@naver.com>
2023-09-21 18:25:23 +0900

    username, menu, userpic 설정

--- ProfileAndCoreData/ProfileAndCoreData/Profile/View/ProfileDesignViewController.swift
+++ ProfileAndCoreData/ProfileAndCoreData/Profile/View/ProfileDesignViewController.swift
@@ -6,11 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class ProfileDesignViewController: UIViewController {
 
+    struct Constraint {
+        static let top10 = 10
+        static let top14 = 14
+        static let trailing16 = -16
+        static let leading14 = 14
+    }
     
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var userPicImageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    func setupUI() {
+        setupUserName()
+        setupMenuButton()
+        setupUserPic()
+    }
+    
+    func setupUserName() {
+        userNameLabel.text = "nabaecamp"
+        
+        userNameLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Constraint.top10)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    func setupMenuButton() {
+        menuButton.setImage(UIImage(named: "Menu"), for: .normal)
+        menuButton.setTitle("", for: .normal)
+        
+        menuButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Constraint.top14)
+            $0.trailing.equalToSuperview().offset(Constraint.trailing16)
+        }
+    }
+    
+    func setupUserPic() {
+        userPicImageView.image = UIImage(named: "Ellipse")
+        
+        userPicImageView.snp.makeConstraints {
+            $0.top.equalTo(userNameLabel.snp.bottom).offset(Constraint.top14)
+            $0.leading.equalToSuperview().offset(Constraint.leading14)
+        }
     }
 }
